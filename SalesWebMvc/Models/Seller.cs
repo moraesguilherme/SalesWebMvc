2e9eea8679597f74530bb8dfbd3697014ee471f1
Guilherme Moraes <moraesguilherme91@gmail.com>
2019-10-29 23:28:09 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,17 +9,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} obrgatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O {0} deve ser entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrgatorio")]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail valido")]
         public string Email { get; set; }
 
-        [Display(Name="Birth Date")]
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} obrgatorio")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Display(Name="Base Salary")]
+        [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} obrgatorio")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve estar entre {1} e {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
